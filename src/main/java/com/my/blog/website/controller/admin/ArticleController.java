ad34bb28c56fea2c266d647871e050dfbb55b4ac
zhisheng17 <1041218129@qq.com>
2017-06-17 16:26:52 +0800

    SpringBoot + Mybatis + thymeleaf 搭建的个人博客

---
+++ src/main/java/com/my/blog/website/controller/admin/ArticleController.java
@@ -0,0 +1,174 @@
+package com.my.blog.website.controller.admin;
+
+
+import com.github.pagehelper.PageInfo;
+import com.my.blog.website.controller.BaseController;
+import com.my.blog.website.dto.LogActions;
+import com.my.blog.website.dto.Types;
+import com.my.blog.website.exception.TipException;
+import com.my.blog.website.modal.Bo.RestResponseBo;
+import com.my.blog.website.modal.Vo.ContentVo;
+import com.my.blog.website.modal.Vo.ContentVoExample;
+import com.my.blog.website.modal.Vo.MetaVo;
+import com.my.blog.website.modal.Vo.UserVo;
+import com.my.blog.website.service.IContentService;
+import com.my.blog.website.service.ILogService;
+import com.my.blog.website.service.IMetaService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by 13 on 2017/2/21.
+ */
+@Controller
+@RequestMapping("/admin/article")
+@Transactional(rollbackFor = TipException.class)
+public class ArticleController extends BaseController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArticleController.class);
+
+    @Resource
+    private IContentService contentsService;
+
+    @Resource
+    private IMetaService metasService;
+
+    @Resource
+    private ILogService logService;
+
+    /**
+     * 文章列表
+     * @param page
+     * @param limit
+     * @param request
+     * @return
+     */
+    @GetMapping(value = "")
+    public String index(@RequestParam(value = "page", defaultValue = "1") int page,
+                        @RequestParam(value = "limit", defaultValue = "15") int limit, HttpServletRequest request) {
+        ContentVoExample contentVoExample = new ContentVoExample();
+        contentVoExample.setOrderByClause("created desc");
+        contentVoExample.createCriteria().andTypeEqualTo(Types.ARTICLE.getType());
+        PageInfo<ContentVo> contentsPaginator = contentsService.getArticlesWithpage(contentVoExample,page,limit);
+        request.setAttribute("articles", contentsPaginator);
+        return "admin/article_list";
+    }
+
+    /**
+     * 文章发表
+     * @param request
+     * @return
+     */
+    @GetMapping(value = "/publish")
+    public String newArticle(HttpServletRequest request) {
+        List<MetaVo> categories = metasService.getMetas(Types.CATEGORY.getType());
+        request.setAttribute("categories", categories);
+        return "admin/article_edit";
+    }
+
+    /**
+     * 文章编辑
+     * @param cid
+     * @param request
+     * @return
+     */
+    @GetMapping(value = "/{cid}")
+    public String editArticle(@PathVariable String cid, HttpServletRequest request) {
+        ContentVo contents = contentsService.getContents(cid);
+        request.setAttribute("contents", contents);
+        List<MetaVo> categories = metasService.getMetas(Types.CATEGORY.getType());
+        request.setAttribute("categories", categories);
+        request.setAttribute("active", "article");
+        return "admin/article_edit";
+    }
+
+    /**
+     * 文章发表
+     * @param contents
+     * @param request
+     * @return
+     */
+    @PostMapping(value = "/publish")
+    @ResponseBody
+    @Transactional(rollbackFor = TipException.class)
+    public RestResponseBo publishArticle(ContentVo contents,  HttpServletRequest request) {
+        UserVo users = this.user(request);
+        contents.setAuthorId(users.getUid());
+        contents.setType(Types.ARTICLE.getType());
+        if (StringUtils.isBlank(contents.getCategories())) {
+            contents.setCategories("默认分类");
+        }
+        try {
+            contentsService.publish(contents);
+        } catch (Exception e) {
+            String msg = "文章发布失败";
+            if (e instanceof TipException) {
+                msg = e.getMessage();
+            } else {
+                LOGGER.error(msg, e);
+            }
+            return RestResponseBo.fail(msg);
+        }
+        return RestResponseBo.ok();
+    }
+
+    /**
+     * 文章更新
+     * @param contents
+     * @param request
+     * @return
+     */
+    @PostMapping(value = "/modify")
+    @ResponseBody
+    @Transactional(rollbackFor = TipException.class)
+    public RestResponseBo modifyArticle(ContentVo contents,HttpServletRequest request) {
+        UserVo users = this.user(request);
+        contents.setAuthorId(users.getUid());
+        contents.setType(Types.ARTICLE.getType());
+        try {
+            contentsService.updateArticle(contents);
+        } catch (Exception e) {
+            String msg = "文章编辑失败";
+            if (e instanceof TipException) {
+                msg = e.getMessage();
+            } else {
+                LOGGER.error(msg, e);
+            }
+            return RestResponseBo.fail(msg);
+        }
+        return RestResponseBo.ok();
+    }
+
+    /**
+     * 删除文章
+     * @param cid
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/delete")
+    @ResponseBody
+    @Transactional(rollbackFor = TipException.class)
+    public RestResponseBo delete(@RequestParam int cid, HttpServletRequest request) {
+        try {
+            contentsService.deleteByCid(cid);
+            logService.insertLog(LogActions.DEL_ARTICLE.getAction(), cid+"", request.getRemoteAddr(), this.getUid(request));
+        } catch (Exception e) {
+            String msg = "文章删除失败";
+            if (e instanceof TipException) {
+                msg = e.getMessage();
+            } else {
+                LOGGER.error(msg, e);
+            }
+            return RestResponseBo.fail(msg);
+        }
+        return RestResponseBo.ok();
+    }
+}
