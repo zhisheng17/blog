fb832c40e296758aeea156818e7e6be05e7f8226
HuangYuhui <Gentleman_0109@outlook.com>
2020-02-07 15:35:50 +0800

    :tada: modify some configuration informations so that run it successfully
    due to my mysql version is : 8.0.11 and java version is :  11.0.4

--- src/main/java/com/my/blog/website/utils/Tools.java
+++ src/main/java/com/my/blog/website/utils/Tools.java
@@ -1,7 +1,8 @@
 package com.my.blog.website.utils;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import org.apache.tomcat.util.codec.binary.Base64;
+//import sun.misc.BASE64Decoder;
+//import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -15,6 +16,7 @@ import java.util.Random;
 /**
  * 工具类
  * Created by BlueT on 2017/3/9.
+ * Modify by HuangYuhui on 2020/2/7 and refer to https://blog.csdn.net/qingshuiwater/article/details/82468758
  */
 public class Tools {
     private static final Random random = new Random();
@@ -57,14 +59,16 @@ public class Tools {
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
         byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-        return new BASE64Encoder().encode(encryptedBytes);
+        //return new BASE64Encoder().encode(encryptedBytes);
+        return Base64.encodeBase64String(encryptedBytes);
     }
 
     public static String deAes(String data, String key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-        byte[] cipherTextBytes = new BASE64Decoder().decodeBuffer(data);
+        //byte[] cipherTextBytes = new BASE64Decoder().decodeBuffer(data);
+        byte[] cipherTextBytes =Base64.decodeBase64(data);
         byte[] decValue = cipher.doFinal(cipherTextBytes);
         return new String(decValue);
     }
